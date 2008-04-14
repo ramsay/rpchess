894be2a02901284125fa3ca41ed28a2332c1665c
durandal <durandal@be83e83a-1b48-0410-b31e-f1b1fab5d1e3>
2008-04-14 21:17:56 +0000

    Made lots of changes to the classes in Model.cs

--- trunk/RPChess/RPChess/Model.cs
+++ trunk/RPChess/RPChess/Model.cs
@@ -7,7 +7,21 @@ namespace RPChess
 {
     enum MoveDirection { Right, FowardRight, Forward, ForwardLeft, 
             Left, BackwardLeft, Backward, BackwardRight };
-    
+    enum MoveType { Capture, Movement, Attack };
+
+    struct BoardVector
+    {
+        public MoveDirection Direction;
+        public int Length;
+
+        public BoardLocation toOffset()
+        {
+            BoardLocation b;
+            b.Y = (int)Math.Sin((double)((int)Direction / 4));
+            b.X = (int)Math.Cos((double)((int)Direction / 4));
+        }
+    }
+
     struct BoardLocation 
     {
         public int X;
@@ -70,10 +84,30 @@ namespace RPChess
 
     class Piece
     {
-        public readonly int MAX_HP;
-        public readonly Ability[] abilityList;
-        public readonly String name;
-        public readonly Move[] moveSet;
+        protected int _MAX_HP;
+        public int MAX_HP
+        {
+            get
+            {
+                return _MAX_HP;
+            }
+        }
+        protected String _name;
+        public String Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        protected Move[] _moveSet;
+        public Move[] MoveSet
+        {
+            get
+            {
+                return _moveSet;
+            }
+        }
         private int _HP;
         public int HP
         {
@@ -83,13 +117,11 @@ namespace RPChess
             }
         }
 
-        public Piece ( String name, int hp, Ability[] aList,
-            Move[] mSet )
+        public Piece ( String name, int hp, Move[] moveSet )
         {
-            this.name = name;
-            MAX_HP = hp;
-            abilityList = aList;
-            moveSet = mSet;
+            _name = name;
+            _MAX_HP = hp;
+            _moveSet = moveSet;
             initialize();
         }
 
@@ -114,50 +146,52 @@ namespace RPChess
             _HP = _HP + (int)heal;
             return _HP;
         }
+
+        public String ToXMLString()
+        {
+            String repr = "<piece name=\"" + _name +
+                               "\"HP=\"" + _MAX_HP +
+                               "\"/>\r\n";
+            foreach ( Move m in _moveSet )
+            {
+                repr << m.ToString();
+            }
+            return repr;
+        }
     }
 
-    class Ability
+    interface Move
     {
-        public readonly int[][] AreaOfEffect;
-        public readonly String Name;
-        public readonly int MAX_POINTS;
-        private int _points;
+        MoveType getType();
+        XmlElement toXML();
+        void fromXML(XmlElement xml);
+    }
+
+    class Attack : Move
+    {
+        protected String _name;
+        public String Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        protected int _MAX_POINTS;
+        public int MAX_POINTS
+        {
+            get
+            {
+                return _MAX_POINTS;
+            }
+        }   
+        protected int _points;
         public int Points
         {
             get
             {
                 return _points;
             }
-        }
-
-        /// <summary>
-        /// Constructor for Ability, sets up the varibles and sets the
-        /// useability points to MAX_POINTS.
-        /// </summary>
-        /// <param name="name" type="String">
-        /// The name to give the ability.
-        /// </param>
-        /// <param name="AreaOfEffect" type="int[][]">
-        /// An array of boundless size that is a value map of the 
-        /// damage or healing the ability creates.
-        /// </param>
-        /// <param name="points" type="int">
-        /// The amount of points.
-        /// </param>
-        public Ability(String name, int[][] areaOfEffect, int points)
-        {
-            Name = name;
-            AreaOfEffect = areaOfEffect;
-            MAX_POINTS = points;
-            initialize();
-        }
-
-        /// <summary>
-        /// Resets the points equal to MAX_POINTS.
-        /// </summary>
-        public void initialize()
-        {
-            _points = MAX_POINTS;
         }
 
         /// <summary>
@@ -169,28 +203,127 @@ namespace RPChess
             _points = _points - 1;
             return _points;
         }
+
+        /// <summary>
+        /// Resets the points to MAX_POINTS.
+        /// </summary>
+        /// <returns>Returns the remaining points.</returns>
+        public int reset()
+        {
+            _points = _MAX_POINTS;
+            return _points;
+        }
+
+        public MoveType getType()
+        {
+            return MoveType.Attack;
+        }
+
+        //public virtual XmlElement toXML();
+        //public virtual void fromXML(XmlElement xml);
     }
 
+    class AreaOfEffectAbility : Attack
+    {
+        private int[][] _areaOfEffect;
+        public int[][] AreaOfEffect
+        {
+            get
+            {
+                return _areaOfEffect;
+            }
+        }
 
+        public AreaOfEffectAbility(String name, int points, int[][] areaOfEffect)
+        {
+            _name = name;
+            _MAX_POINTS = points;
+            _areaOfEffect = areaOfEffect;
+            reset();
+        }
+
+        public XmlElement toXML()
+        {
+            XmlElement xml;
+            return xml;
+        }
+
+        public void fromXML(XmlElement xml)
+        {
+        }
+    }
+
+    class DirectionalAbility : Attack
+    {
+        private BoardVector _boardVector;
+        public BoardVector BoardVector
+        {
+            get
+            {
+                return _boardVector;
+            }
+        }
+        private int _damage;
+        public int Damage
+        {
+            get
+            {
+                return _damage;
+            }
+        }
+
+
+        public DirectionalAbility(String name, int points, BoardVector boardVector)
+        {
+            _name = name;
+            _MAX_POINTS = points;
+            _boardVector = boardVector;
+        }
+
+        public DirectionalAbility(XmlElement xml)
+        {
+            fromXML(xml);
+        }
+
+        public XmlElement toXML()
+        {
+            XmlElement xml;
+            return xml;
+        }
+
+        public void fromXML(XmlElement xml)
+        {
+        }
+
+        public String ToXMLString()
+        {
+            String repr = "<attack name=\"" + _name +
+                "\" direction=\"" + _boardVector.Direction +
+                "\" length=" + _boardVector.Length +
+                "\" damage=" + _damage +
+                "\"/>";
+            return repr;
+        }
+    }
     /// <summary>
     /// Class level summary documentation goes here.</summary>
     /// <remarks>
     /// Longer comments can be associated with a type or member 
     /// through the remarks tag</remarks>
-    class Move
+    class Movement : Move
     {
         private readonly MoveDirection Direction;
         private readonly int Length;
         public readonly int Forward;
         public readonly int Right;
 
-        public Move(int forward, int right)
+        public Movement(int forward, int right)
         {
             Forward = forward;
             Right = right;
         }
 
-        public Move(MoveDirection direction, int length)
+        public Movement(MoveDirection direction, int length)
         {
             Direction = direction;
             Length = length;
@@ -212,16 +345,9 @@ namespace RPChess
             Right = right;
         }
 
-        public Move(XmlReader xml)
+        public Movement(XmlReader xml)
         {
-            if (xml.LocalName.ToLower().Equals("move"))
-            {
-                Direction = (MoveDirection) Enum.Parse( typeof(MoveDirection), 
-                    xml.GetAttribute("Direction"));
-                Length = Int32.Parse(xml.GetAttribute("Length"));
-                Forward = Int32.Parse(xml.GetAttribute("Forward"));
-                Right = Int32.Parse(xml.GetAttribute("Right"));
-            }
+            fromXML(xml);
         }
 
         public BoardLocation moveFrom(BoardLocation bLoc)
@@ -229,6 +355,21 @@ namespace RPChess
             bLoc.X += Right;
             bLoc.Y += Forward;
             return bLoc;
+        }
+
+        public MoveType getType()
+        {
+            return MoveType.Movement;
+        }
+
+        public XmlElement toXML()
+        {
+            XmlElement xml;
+            return xml;
+        }
+
+        public void fromXML(XmlElement xml)
+        {
         }
     }
 }
