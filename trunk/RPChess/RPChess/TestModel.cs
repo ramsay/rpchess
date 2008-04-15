27462c4db5ec492e4504aeee943c8a787e9a9cfc
durandal <durandal@be83e83a-1b48-0410-b31e-f1b1fab5d1e3>
2008-04-15 22:50:13 +0000

    Got Nunit working.

--- trunk/RPChess/RPChess/TestModel.cs
+++ trunk/RPChess/RPChess/TestModel.cs
@@ -5,26 +5,36 @@ using System.Text;
 namespace RPChess
 {
     using NUnit.Framework;
-    
+
+
+    ///<summary>
+    ///Tests the Movement class.
+    ///</summary>
     [TestFixture]
-    class MovementTest
+    public class MovementTest
     {
         readonly BoardLocation start;
-        [Setup]
+
+        ///<summary>
+        ///Tests the Movement class.
+        ///</summary>
         public MovementTest()
         {
             start.X = 0;
             start.Y = 0;
         }
 
+        ///<summary>
+        ///Tests the Movement class.
+        ///</summary>
         [Test]
         public void moveFrom()
         {
             Movement move = new Movement(1, 0, false);
             BoardLocation test;
             test = move.moveFrom(start);
-            Assert(0, test.X);
-            Assert(1, test.Y);
+            Assert.AreEqual(0, test.X);
+            Assert.AreEqual(1, test.Y);
         }
     }
 }
