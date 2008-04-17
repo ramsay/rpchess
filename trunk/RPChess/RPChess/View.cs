600442de1251a580b354d29303916aa1c58610ac
durandal <durandal@be83e83a-1b48-0410-b31e-f1b1fab5d1e3>
2008-04-17 19:02:35 +0000

    Filling in some methods for View class.

--- trunk/RPChess/RPChess/View.cs
+++ trunk/RPChess/RPChess/View.cs
@@ -4,20 +4,64 @@ using System.Text;
 
 namespace RPChess
 {
+	enum ViewType { Text, TwoD, ThreeD };
     interface View
     {
-
-        void update( Log movelog );
+		ViewType Type;
+		long LastMove;
+		double LastTime;
+		void initialize;
+        void update( Log movelog, BoardSpace[] boardState);
     }
 
     class TextView : View
     {
+		long _lastMove;
+		public LastMove
+		{
+			get
+			{
+				return _lastMove;
+			}
+		}
+		double _lastTime;
+		public LastTime
+		{
+			get
+			{
+				return _lastTime;
+			}
+		}
+		ViewType Type
+		{
+			get
+			{
+				return ViewType.Text;
+			}
+		}
         public TextView()
         {
         }
-
-        public void update(Log movelog)
+		
+		public void initialize()
+		{
+			_lastMove = 0;
+			_lastTime = 0;
+			// set Buffer size (length, width)
+			// set window size (length, width)
+			// set Background color
+			// set foreground color
+			// set Window Title
+		}
+		
+        public void update(Log movelog, BoardSpace[] boardState)
         {
+			String board = _boardToString();
+			Console.Clear();
+			Console.Write(board);
+			Console.Write("Next Move>");
+			_lastMove = movelog.size();
+			_lastTime = DateTime.Now();
         }
     }
 }
