dac44771ea8becabf4b00b3ea7bd2b86c8723d61
durandal <durandal@be83e83a-1b48-0410-b31e-f1b1fab5d1e3>
2008-04-18 19:54:17 +0000

    Turning Log.cs into a Monostate ( variation on singleton ).

--- trunk/RPChess/RPChess/Log.cs
+++ trunk/RPChess/RPChess/Log.cs
@@ -5,24 +5,16 @@ using System.Text;
 namespace RPChess
 {
     /// <summary>
-    /// A lazy mans message storage / callback.
+    /// A lazy man's message storage / callback.
     /// The Log class maintains a list of all moves made
-    /// throughout the game. 
+    /// throughout the game. The list is static making 
+	/// this a monostate.
     /// </summary>
     public class Log
     {
-        private List<String> _moveList;
-        /// <summary>
-        /// An immutable array of Strings that allows
-        /// all other classes in the RPChess
-        /// </summary>
-        public String[] MoveList
-        {
-            get
-            {
-                return _moveList.ToArray();
-            }
-        }
+        private static List<String> _moveList;
+        private static uint _references;
+        private static bool _initialized;
         /// <summary>
         /// The number of moves in the Log.
         /// </summary>
@@ -38,6 +30,7 @@ namespace RPChess
         /// </summary>
         public Log()
         {
+            _references++;
         }
         /// <summary>
         /// Constructor that also initializes.
@@ -45,9 +38,22 @@ namespace RPChess
         /// <param name="init">Initializes if true</param>
         public Log(bool init)
         {
+            _references++;
             if (init)
             {
                 initialize();
+            }
+        }
+        /// <summary>
+        /// Default destructor. Removes a reference. If 
+        /// reference is less than 1 then clear _moveList.
+        /// </summary>
+        public ~Log()
+        {
+            _references--;
+            if (_references < 1)
+            {
+                _moveList.Clear();
             }
         }
         /// <summary>
@@ -55,12 +61,18 @@ namespace RPChess
         /// </summary>
         public void initialize()
         {
-            if (_moveList.Count != 0)
+            if ( _references < 2 || !_initialized)
             {
-                _moveList.Clear();
+                if (_moveList.Count != 0)
+                {
+                    _moveList.Clear();
+                }
+                _moveList = new List<String>();
+                _initialized = true;
             }
-            _moveList = new List<String>();
         }
+        /// <summary>
+        /// The 
         /// <summary>
         /// Adds a move to the Log.
         /// </summary>
@@ -80,5 +92,30 @@ namespace RPChess
         {
             return _moveList[_moveList.Count-1];
         }
+        /// <summary>
+        /// Clears the log.
+        /// </summary>
+        /// <returns>Returns the number of entries cleared.</returns>
+        public int Clear()
+        {
+            if ( _references < 2 )
+            {
+                int count = _moveList.Count;
+                _moveList.Clear();
+                return count;
+            }
+        }
+		/// <summary>
+		/// Index access to all elements in the log.
+		/// </summary>
+		/// <returns>A string located at the index.</returns>
+		public String operator [](int index)
+		{
+			if ( i < 0 || i >= Count || Count == 0)
+			{
+				return "";
+			}
+			return _moveList[index];
+		}
     }
 }
