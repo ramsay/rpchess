d3095f745efcf11dd2e13211809fb00eaaded502
durandal <durandal@be83e83a-1b48-0410-b31e-f1b1fab5d1e3>
2009-12-05 15:00:46 +0000

    Added Project File and Moved IBoardSpace interface to it's own file. chesswar and chesswar-test compile with xbuild.

---
+++ chesswar/Chesswar Implementation/IBoardSpace.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Special.cs" company="BENTwerx">
+//     GPLv3 Copyright 2008 Robert Ramsay
+// </copyright>
+// <author>Robert Ramsay</author>
+//-----------------------------------------------------------------------
+
+namespace chesswar
+{
+    using System.Xml;
+    using System.Xml.Schema;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// An interface to hold empty board spaces and pieces.
+    /// </summary>
+    public interface IBoardSpace : IXmlSerializable
+    {
+        /// <summary>
+        /// Gets a value indicating whether this IBoardSpace is empty.
+        /// </summary>
+        bool IsEmpty
+        {
+            get;
+        }
+		
+    }
+}
